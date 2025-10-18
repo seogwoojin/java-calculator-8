2fc2698b6e610255d8107341ae1204f6f7536c14
woojin <server@uos.ac.kr>
2025-10-18 18:03:59 +0900

    feat(Tokenizer): 입력 문자열을 구분자로 분리하는 기능 구현

--- src/main/java/calculator/domain/parser/Tokenizer.java
+++ src/main/java/calculator/domain/parser/Tokenizer.java
@@ -1,10 +1,16 @@
 package calculator.domain.parser;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 class Tokenizer {
     static List<String> split(String input, Set<String> delimiters) {
-        return List.of();
+        if (input.isBlank()) {
+            return List.of();
+        }
+
+        String regex = String.join("|", delimiters);
+        return Arrays.asList(input.split(regex));
     }
 }
