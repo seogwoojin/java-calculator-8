898cad4ca921f39d45385f5f2ae0148327e5ac9a
woojin <server@uos.ac.kr>
2025-10-20 15:55:04 +0900

    feat: 입출력 처리 기능 구현

---
+++ src/main/java/calculator/external/InputView.java
@@ -0,0 +1,9 @@
+package calculator.external;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String readExpression() {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/calculator/external/OutputView.java
@@ -0,0 +1,7 @@
+package calculator.external;
+
+public class OutputView {
+    public void printResult(int result) {
+        System.out.printf("결과 : %d%n", result);
+    }
+}
